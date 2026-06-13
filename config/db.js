f7adbca313e95492e837aeec7ce02ffb0b0c3a03
vignesh23-developer <vicky14552@gmail.com>
2026-06-13 10:59:57 +0530

    db.js update

--- config/db.js
+++ config/db.js
@@ -3,24 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();
 
-db.getConnection((err, connection) => {
-  if (err) {
-    console.log("Database Connection Failed");
-    console.log(err);
-  } else {
+db.getConnection()
+  .then((connection) => {
     console.log("MySQL Connected.....");
     connection.release();
-  }
-});
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed");
+    console.log(err);
+  });
 
 export default db;
